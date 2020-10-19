5582232af4820a8cc33e58b56a3e0b9a4163bbd1
xita Rps <xitarps@gmail.com>
2020-10-19 17:18:42 -0300

    Set i18n

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,13 @@ module MessageMe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
     config.time_zone = "America/Sao_Paulo"
     config.active_record.default_timezone = "America/Sao_Paulo"
     ENV["TZ"] = "America/Sao_Paulo"
+    
+    I18n.available_locales = [:en, :'pt-BR']
+    config.i18n.default_locale = :'pt-BR'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
