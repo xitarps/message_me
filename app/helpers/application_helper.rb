68e20b81797e8c8a5391dd482c30419da259d2e0
xita Rps <xitarps@gmail.com>
2020-10-20 00:07:42 -0300

    Add gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    hash = Digest::MD5.hexdigest(user.email)
+    size = options[:size]
+    image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag image_src, alt: user.username, class:"rounded shadow mx-auto d-block round-custom-img"
+  end
 end
